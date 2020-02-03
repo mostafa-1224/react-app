0e81d79fedc4f3ec734e0c8a1ac05562fb1083c5
mustafa mahmoud <31194057+mostafa-1224@users.noreply.github.com>
2020-02-03 22:24:08 +0200

    Updates

---
+++ precache-manifest.22d1c0a8a0c66e9ff23f3a8f09644135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a43260c40dce58082d1aa398ec18f38",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "1b2ad2b90cdfa31bf1fb",
+    "url": "/react-app/static/css/main.c8a4ffed.chunk.css"
+  },
+  {
+    "revision": "27227d9b94f3d39b5a02",
+    "url": "/react-app/static/js/2.7c4ecbe3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app/static/js/2.7c4ecbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2ad2b90cdfa31bf1fb",
+    "url": "/react-app/static/js/main.80b38ed3.chunk.js"
+  },
+  {
+    "revision": "67181f99d935735d4a41",
+    "url": "/react-app/static/js/runtime-main.10b708de.js"
+  }
+]);
